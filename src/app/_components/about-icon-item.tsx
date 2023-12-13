ad366394b9c8b3f3c550df845631ddb79d8298e9
Nurhamsah <nur.hamsah.cash@gmail.com>
2023-12-13 17:08:42 +0700

    .

--- src/app/_components/about-icon-item.tsx
+++ src/app/_components/about-icon-item.tsx
@@ -1,18 +1,24 @@
 'use client';
 
-import { COLOR_PRIMARY, COLOR_PRIMARY_LIGHTER } from '@/constants/ui';
+import { COLOR_PRIMARY } from '@/constants/ui';
 import { Box } from '@chakra-ui/react';
 import Image from 'next/image';
 import React from 'react';
 
 function AboutIconItem({ icon, alt }: { icon: string; alt: string }) {
   return (
-    <Box className='tw-p-[.5rem] tw-rounded-full' bgColor={'primary.50'}>
+    <Box className='tw-rounded-full tw-p-[.5rem]' bgColor={'primary.50'}>
       <div
-        className='tw-p-[.5rem] sm:tw-p-[.875rem] tw-rounded-full'
+        className='tw-rounded-full tw-p-[.5rem] sm:tw-p-[.875rem]'
         style={{ backgroundColor: COLOR_PRIMARY }}
       >
-        <Image src={icon} alt={alt} className='tw-min-w-[18px] sm:tw-min-w-[24px] tw-min-h-[18px] sm:tw-min-h-[24px]' width={24} height={24} />
+        <Image
+          src={icon}
+          alt={alt}
+          className='tw-min-h-[18px] tw-min-w-[18px] sm:tw-min-h-[24px] sm:tw-min-w-[24px]'
+          width={24}
+          height={24}
+        />
       </div>
     </Box>
   );
