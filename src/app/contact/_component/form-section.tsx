fbd2d27e7e4da7b4bf10f34ea5e0f946279531d9
abdmalik-sya <abdmaliksyarifudin@gmail.com>
2024-01-09 12:07:54 +0700

    fixing: reload page & set alert

--- src/app/contact/_component/form-section.tsx
+++ src/app/contact/_component/form-section.tsx
@@ -12,6 +12,7 @@ import {
   Textarea,
   VStack,
 } from '@chakra-ui/react';
+import { useToast } from '@chakra-ui/react'
 import axios from "axios";
 import Image from 'next/image';
 import React from 'react';
@@ -22,18 +23,53 @@ function FormSection() {
   const [phone, setPhone] = React.useState('');
   const [subject, setSubject] = React.useState('');
   const [message, setMessage] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const toast = useToast()
 
   const urlAPI = "https://api.elasticemail.com/v2/email/send?"
   const apiKey = "apikey=D1E7F3974433F1828E1334EFF88B1ABBF21BE069A62CEE700D9361EACD3BE3F0F0B0C9B07F3D496321D0013C9737B52A&"
   const apiFrom = "no-reply@kellnersimora.com"
+  const apiTo = "support@kellnersimora.com"
   const apiIsTransactional = "true"
+  const apiMassageValue = "<table><tr><td><b>Nama</b></td><td>: "+name+"</td></tr><tr><td><b>Telepon</b></td><td>: "+phone+"</td></tr><tr><td><b>Email</b></td><td>: "+email+"</td></tr><tr><td><b>Isi Pesan</b></td><td>: </td></tr></table><p>"+message+"</p>"
   const handle = async (e:any) => {
-    await axios.post(urlAPI+apiKey+"subject="+subject+"&from="+apiFrom+"&to=support@kellnersimora.com&bodyText="+message+"&isTransactional="+apiIsTransactional+"&bodyHtml=<p>"+"Nama: "+name+"<br />"+"Telepon: "+phone+"<br />"+"Pesan: "+"<br />"+message+"</p>")
+    setLoading(true)
+    await axios.post(urlAPI+apiKey+"subject="+subject+"&from="+apiFrom+"&to="+apiTo+"&bodyText="+message+"&isTransactional="+apiIsTransactional+"&bodyHtml="+apiMassageValue)
     .then(function (response) {
-      alert("Pesan terkirim");
+      if (response.data.success) {
+        setLoading(false)
+        setName('')
+        setEmail('')
+        setPhone('')
+        setSubject('')
+        setMessage('')
+        toast({
+          title: 'Berhasil',
+          description: "Pesan anda telah terkirim.",
+          status: 'success',
+          duration: 1000,
+          isClosable: true,
+        })
+      }else{
+        toast({
+          title: 'Gagal',
+          description: "Silahkan isi form dengan sesuai.",
+          status: 'error',
+          duration: 1000,
+          isClosable: true,
+        })
+      }
+      
     })
     .catch(function (error) {
-      alert("Pesan gagal dikirim");
+      console.log(error);      
+      toast({
+        title: 'Gagal',
+        description: error,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
     });
   }
 
@@ -51,7 +87,6 @@ function FormSection() {
         <Text fontSize={'1.25rem'} color={COLOR_GRAY_500}>
           Tim kami dengan senang hati akan membantu Anda.
         </Text>
-        <form onSubmit={handle}>
         <VStack pt={'3rem'} gap={'1.5rem'} alignItems={'stretch'}>
           <Box
             display={'flex'}
@@ -60,20 +95,20 @@ function FormSection() {
           >
             <FormControl isRequired>
               <FormLabel color={COLOR_TEXT}>Nama</FormLabel>
-              <Input type='text' placeholder='Masukkan Nama Anda' onChange={e => setName(e.target.value)} />
+              <Input type='text' placeholder='Masukkan Nama Anda' value={name} onChange={e => setName(e.target.value)} />
             </FormControl>
             <FormControl isRequired>
               <FormLabel color={COLOR_TEXT}>Email</FormLabel>
-              <Input type='email' placeholder='Masukkan Email Anda' onChange={e => setEmail(e.target.value)} />
+              <Input type='email' placeholder='Masukkan Email Anda' value={email} onChange={e => setEmail(e.target.value)} />
             </FormControl>
           </Box>
           <FormControl>
             <FormLabel color={COLOR_TEXT}>Telepon (opsional)</FormLabel>
-            <Input type='number' placeholder='Masukkan Nomor Telepon Anda' onChange={e => setPhone(e.target.value)} />
+            <Input type='number' placeholder='Masukkan Nomor Telepon Anda' value={phone} onChange={e => setPhone(e.target.value)} />
           </FormControl>
           <FormControl isRequired>
             <FormLabel color={COLOR_TEXT}>Topik</FormLabel>
-            <Input type='text' placeholder='Masukkan Topik' onChange={e => setSubject(e.target.value)} />
+            <Input type='text' placeholder='Masukkan Topik' value={subject} onChange={e => setSubject(e.target.value)} />
           </FormControl>
           <FormControl>
             <FormLabel color={COLOR_TEXT}>Pesan (opsional)</FormLabel>
@@ -90,6 +125,7 @@ function FormSection() {
               }}
               placeholder='Masukkan Isi Pesan Anda'
               size='sm'
+              value={message}
               onChange={e => setMessage(e.target.value)}
             />
           </FormControl>
@@ -98,11 +134,12 @@ function FormSection() {
             colorScheme='primary'
             style={{ background: COLOR_PRIMARY }}
             type='submit'
+            onClick={handle}
+            isLoading={loading}
           >
             Kirim
           </Button>
         </VStack>
-        </form>
       </div>
     </div>
   );
